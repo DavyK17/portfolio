9a94e1ddcb5ac566868db614c1bed5ab3c934680
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-08-02 13:51:20 +0300

    Updating icon

--- src/assets/img/icons/Icons.js
+++ src/assets/img/icons/Icons.js
@@ -1,13 +1,12 @@
 import email from "./email.svg";
 import github from "./github.svg";
-import icon from "./icon.png";
 import kuhustle from "./kuhustle.png";
 import linkedin from "./linkedin.svg";
 import menu from "./menu.svg";
 import upwork from "./upwork.svg";
 
 const Icons = {
-    email, github, icon, kuhustle, linkedin, menu, upwork
+    email, github, kuhustle, linkedin, menu, upwork
 }
 
 export default Icons;
