b6406610fc8e33d2cd500c674f46bd03963e5787
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-11-24 11:26:14 +0300

    Adding Netlify CMS entry

--- src/assets/img/dev/Dev.js
+++ src/assets/img/dev/Dev.js
@@ -5,11 +5,12 @@ import html from "./html5.svg";
 import js from "./javascript-1.svg";
 import jekyll from "./jekyll.svg";
 import jquery from "./jquery-1.svg";
+import netlifyCMS from "./netlify-cms.svg";
 import node from "./nodejs-1.svg";
 import react from "./react.svg";
 
 const Dev = {
-    bootstrap, css, greensock, html, js, jekyll, jquery, node, react
+    bootstrap, css, greensock, html, js, jekyll, jquery, netlifyCMS, node, react
 }
 
 export default Dev;
