9eedc29af79a5857d20cbf14ddab0bd745138232
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2022-04-01 12:26:04 +0300

    Editing intro, languages and libraries/systems

--- src/body/Intro.js
+++ src/body/Intro.js
@@ -10,11 +10,10 @@ function Intro() {
                 <div className="brief">
                     <h2>Web developer</h2>
                     <p>A professional who always strives to do his work with diligence and precision, so you can be sure there will be no errors in the code or the functionality of the website. Having done editing and proofreading work, I also have a very strong
-                        command of the English language and speak English at a native level, so you can be sure there will be no errors in spelling, grammar or terminology with the content of the site either. Currently extending my knowledge to other front-end
-                        technologies such as React as well as the back end to transition into a full-stack developer.
+                        command of the English language and speak English at a native level, so you can be sure there will be no errors in spelling, grammar or terminology with the content of the site either. Currently extending my knowledge to the back-end as I transition into a full-stack developer.
                     </p>
-                    <p><strong>Languages:</strong> HTML, CSS, JavaScript, Liquid, PHP</p>
-                    <p><strong>Libraries/Systems:</strong> Bootstrap, GreenSock, Jekyll, jQuery, Node.js, React, Redux, Wordpress</p>
+                    <p><strong>Languages:</strong> HTML, CSS, JavaScript, Liquid, SQL</p>
+                    <p><strong>Libraries/Systems:</strong> Bootstrap, GreenSock, Jekyll, jQuery, Node.js, PostgreSQL, React, Redux</p>
                 </div>
             </div>
         </div>
