5b0587faa504fbe059376b4121938eb0dbdd5eb6
Davy Kamanzi <davykamanzi@gmail.com>
2025-04-12 02:05:40 +0300

    Fleshing out layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
 	import "../app.css";
+
+	import Nav from "$lib/components/Nav.svelte";
+	import Footer from "$lib/components/Footer.svelte";
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div id="root">
+	<Nav />
+	<main>
+		<h1 class="sr-only">Davy Kamanzi - Portfolio</h1>
+		<div class="mobile-filler"></div>
+		{@render children()}
+	</main>
+	<Footer />
+</div>
