f9f0a4c8ca10bdc9272461d8541f3e49834fa532
Davy Kamanzi <davykamanzi@gmail.com>
2025-04-12 16:19:26 +0300

    Fleshing out home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Bio from "$lib/img/bio.webp";
+</script>
+
+<h1 class="uppercase">Davy Kamanzi</h1>
+<div class="flex max-w-4xl max-md:flex-col max-md:items-center md:flex-row">
+	<img class="h-60 border-2 border-solid border-black" src={Bio} alt="Davy Kamanzi" />
+	<div class="px-4">
+		<h2>Web developer</h2>
+		<p>
+			Full-stack engineer delivering robust, error-resistant applications. Proficient in HTML5, CSS3, JavaScript (ES6+), and
+			SQL, with a particular focus on SvelteKit, Supabase, and PostgreSQL. Dedicated to sharpening my skills by learning
+			from seasoned engineers and exploring new technologies.
+		</p>
+		<ul class="my-2 list-none p-0">
+			<li><strong>Languages:</strong> HTML, CSS, JavaScript, Liquid, SQL</li>
+			<li><strong>Frameworks/Systems:</strong></li>
+		</ul>
+	</div>
+</div>
