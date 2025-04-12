4b24d981a67c46dd187c8ab8013b42082d350a41
Davy Kamanzi <davykamanzi@gmail.com>
2025-04-12 20:50:30 +0300

    Adding contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { contacts } from "$lib/helpers";
+</script>
+
+<h2 class="uppercase">Contact</h2>
+<p>Get in touch today to get started on your project.</p>
+<div class="contact-icons">
+	{#each contacts as { name, link, logo } (name)}
+		<a href={link} target="_blank" rel="noreferrer" title={name}><svelte:component this={logo} aria-label={name} /></a>
+	{/each}
+</div>
