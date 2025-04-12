8cc2be408f0bd043c7b9a0576a1f50aa256988aa
Davy Kamanzi <davykamanzi@gmail.com>
2025-04-12 19:53:27 +0300

    Adding error pages

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from "$app/state";
+
+	const message =
+		page.status >= 500 && page.status <= 599
+			? "An internal server error occurred. Kindly refresh the page or try again later."
+			: page.status === 404
+				? "The requested resource was not found."
+				: page.error?.message;
+</script>
+
+<h2 class="uppercase">{page.status} error</h2>
+<p>{message}</p>
