13cafae9670e4a5cd419c794f182e58aa209942e
Davy Kamanzi <davykamanzi@gmail.com>
2025-04-12 18:53:09 +0300

    Splitting dev and projects objects

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -25,42 +25,42 @@ interface DevItem {
 interface Project {
 	title: string;
 	desc: string[];
-	site: string;
-	skills: string[];
+	site?: string;
+	repo?: string;
+	skills: DevItem[];
 }
 
 /* Helpers */
-export const Helpers = {
-	dev: {
-		aws: { name: "Amazon Web Services", logo: AWS },
-		bootstrap: { name: "Bootstrap", logo: Bootstrap },
-		deno: { name: "Deno", logo: Deno },
-		gcp: { name: "Google Cloud Platform", logo: GCP },
-		gsap: { name: "GSAP", logo: GSAP },
-		jekyll: { name: "Jekyll", logo: Jekyll },
-		postgres: { name: "PostgreSQL", logo: Postgres },
-		sass: { name: "Sass", logo: Sass },
-		supabase: { name: "Supabase", logo: Supabase },
-		sveltekit: { name: "SvelteKit", logo: Svelte },
-		tailwindcss: { name: "Tailwind CSS", logo: TailwindCSS }
-	} as Dev,
-	projects: [
-		{
-			title: "Arkad World",
-			desc: [
-				"Arkad World Ltd is a Kenyan company specialising in conference interpreting, translation and conference management services. I was tasked with overhauling the company's website by removing Wordpress and building a simple, new design."
-			],
-			site: "https://arkadworld.com/",
-			skills: ["jekyll", "sass", "gsap"]
-		},
-		{
-			title: "Koloseum: Public Authentication",
-			desc: [
-				"Koloseum is an online esports platform currently in active development that connects players, gaming lounges, and esports fans. I am overseeing the development of all the platform's microservices starting with this one, which allows users to create a new account or log into their existing account.",
-				"All of the platform's microservices are to be built using the listed frameworks and systems."
-			],
-			site: "https://auth.koloseum.ke/",
-			skills: ["sveltekit", "supabase", "postgres", "tailwindcss", "deno"]
-		}
-	] as Project[]
+export const dev: Dev = {
+	aws: { name: "Amazon Web Services", logo: AWS },
+	bootstrap: { name: "Bootstrap", logo: Bootstrap },
+	deno: { name: "Deno", logo: Deno },
+	gcp: { name: "Google Cloud Platform", logo: GCP },
+	gsap: { name: "GSAP", logo: GSAP },
+	jekyll: { name: "Jekyll", logo: Jekyll },
+	postgres: { name: "PostgreSQL", logo: Postgres },
+	sass: { name: "Sass", logo: Sass },
+	supabase: { name: "Supabase", logo: Supabase },
+	sveltekit: { name: "SvelteKit", logo: Svelte },
+	tailwindcss: { name: "Tailwind CSS", logo: TailwindCSS }
 };
+
+export const projects: Project[] = [
+	{
+		title: "Arkad World",
+		desc: [
+			"Arkad World Ltd is a Kenyan company specialising in conference interpreting, translation and conference management services. I was tasked with overhauling the company's website by removing Wordpress and building a simple, new design."
+		],
+		site: "https://arkadworld.com/",
+		skills: [dev.jekyll, dev.sass, dev.gsap]
+	},
+	{
+		title: "Koloseum: Public Authentication",
+		desc: [
+			"Koloseum is an online esports platform currently in active development that connects players, gaming lounges, and esports fans. I am overseeing the development of all the platform's microservices starting with this one, which allows users to create a new account or log into their existing account.",
+			"All of the platform's microservices are to be built using the listed frameworks and systems."
+		],
+		site: "https://auth.koloseum.ke/",
+		skills: [dev.sveltekit, dev.supabase, dev.postgres, dev.tailwindcss, dev.deno]
+	}
+];
